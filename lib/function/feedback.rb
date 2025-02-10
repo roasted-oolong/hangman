6f403ae187953512f37e8f7b394965081cce4fcf
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-02-10 22:47:01 +0000

    Move argument (word) to verify method
    The feedback module had an argument passed into it, which caused an error.

--- lib/function/feedback.rb
+++ lib/function/feedback.rb
@@ -1,7 +1,7 @@
 require_relative 'game_config'
 
-module Feedback(word)
-  def self.verify
+module Feedback
+  def self.verify(word)
     if word.length < GameConfig::MIN_LETTERS || word.length > GameConfig::MAX_LETTERS
       false
     else
